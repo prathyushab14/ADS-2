794ddb77180748436bd88e6d5aee1ff8dda44de1
prathyushab14 <prathyushabathula14@msitprogram.net>
2018-10-30 17:58:49 +0530

    "ads2_m2_a1_1: 3/24 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 0/1 "

--- ADS-2-assignments/2.1 CC_Percolation/Solution.java
+++ ADS-2-assignments/2.1 CC_Percolation/Solution.java
@@ -96,7 +96,7 @@ class Solution {
     }
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
-		int n = Integer.parseInt(sc.nextLine());
+		int n = sc.nextInt();
 		Graph gph = new Graph(n);
 		Percolation p = new Percolation(n);
         while (sc.hasNext()) {
