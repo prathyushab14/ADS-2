457da6ba052b833bcd5b8485a55b36c2435b4d49
prathyushab14 <prathyushabathula14@msitprogram.net>
2018-10-31 20:30:31 +0530

    "ads2_m3_a2_1: 0/5 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 0/1 "

--- ADS-2-assignments/m3/3.2 Bipartite/Graph.java
+++ ADS-2-assignments/m3/3.2 Bipartite/Graph.java
@@ -1,155 +1,120 @@
-/*************************************************************************
- *  Compilation:  javac Graph.java        
- *  Execution:    java Graph input.txt
- *  Dependencies: Bag.java In.java StdOut.java
- *  Data files:   http://algs4.cs.princeton.edu/41undirected/tinyG.txt
- *
- *  A graph, implemented using an array of sets.
- *  Parallel edges and self-loops allowed.
- *
- *  % java Graph tinyG.txt
- *  13 vertices, 13 edges 
- *  0: 6 2 1 5 
- *  1: 0 
- *  2: 0 
- *  3: 5 4 
- *  4: 5 6 3 
- *  5: 3 4 0 
- *  6: 0 4 
- *  7: 8 
- *  8: 7 
- *  9: 11 10 12 
- *  10: 9 
- *  11: 9 12 
- *  12: 11 9 
- *
- *  % java Graph mediumG.txt
- *  250 vertices, 1273 edges 
- *  0: 225 222 211 209 204 202 191 176 163 160 149 114 97 80 68 59 58 49 44 24 15 
- *  1: 220 203 200 194 189 164 150 130 107 72 
- *  2: 141 110 108 86 79 51 42 18 14 
- *  ...
- *  
- *************************************************************************/
-
-
 /**
- *  The <tt>Graph</tt> class represents an undirected graph of vertices
- *  named 0 through V-1.
- *  It supports the following operations: add an edge to the graph,
- *  iterate over all of the neighbors adjacent to a vertex.
- *  Parallel edges and self-loops are permitted.
- *  <p>
- *  For additional documentation, see <a href="http://algs4.cs.princeton.edu/51undirected">Section 5.1</a> of
- *  <i>Algorithms, 4th Edition</i> by Robert Sedgewick and Kevin Wayne.
+ * Class for graph theory.
  */
-public class Graph {
-    private final int V;
-    private int E;
+class Graph {
+    /**
+     * vertices.
+     */
+    private int v;
+    /**
+     * edges.
+     */
+    private int e;
+    /**
+     * array of bag type.
+     */
     private Bag<Integer>[] adj;
-    
-   /**
-     * Create an empty graph with V vertices.
+    /**
+     * Constructs the object.
      */
-    public Graph(int V) {
-        if (V < 0) throw new RuntimeException("Number of vertices must be nonnegative");
-        this.V = V;
-        this.E = 0;
-        adj = (Bag<Integer>[]) new Bag[V];
-        for (int v = 0; v < V; v++) {
-            adj[v] = new Bag<Integer>();
+    Graph() {
+    }
+    /**
+     * Constructs the object.
+     *
+     * @param      v1    The v1
+     */
+    Graph(final int v1) {
+        this.v = v1;
+        this.e = 0;
+        adj = (Bag<Integer>[]) new Bag[v1];
+        for (int i = 0; i < v; i++) {
+            adj[i] = new Bag<Integer>();
         }
     }
-
-   /**
-     * Create a random graph with V vertices and E edges.
-     * Expected running time is proportional to V + E.
+    /**
+     * Gets the v.
+     *
+     * @return     The v.
      */
-    public Graph(int V, int E) {
-        this(V);
-        if (E < 0) throw new RuntimeException("Number of edges must be nonnegative");
-        for (int i = 0; i < E; i++) {
-            int v = (int) (Math.random() * V);
-            int w = (int) (Math.random() * V);
-            addEdge(v, w);
+    public int getV() {
+        return v;
+    }
+    /**
+     * Gets the e.
+     *
+     * @return     The e.
+     */
+    public int getE() {
+        return e;
+    }
+    /**
+     * get bag array.
+     *
+     * @return     list
+     */
+    public Bag<Integer>[] getadj() {
+        return adj;
+    }
+    /**
+     * returns number of vertices.
+     *
+     * @return    no of vertices
+     */
+    public int vertices() {
+        return v;
+    }
+    /**
+     * returns number of edges.
+     *
+     * @return    no of edges
+     */
+    public int edges() {
+        return e;
+    }
+    /**
+     * Adds an edge.
+     * complexity of method is O(1)
+     *
+     * @param      v1    integer
+     * @param      w1     integer
+     */
+    public void addEdge(final int v1, final int w1) {
+        if (v1 == w1) {
+            return;
+        }
+        if (!hasEdge(v1, w1)) {
+            e++;
+        adj[v1].add(w1);
+        adj[w1].add(v1);
         }
     }
-
-   /**  
-     * Create a digraph from input stream.
+    /**
+     * returns all the values in list.
+     * complexity of this method is O(1)
+     *
+     * @param      v1     integer
+     *
+     * @return   list.
      */
-/*    public Graph(In in) {
-        this(in.readInt());
-        int E = in.readInt();
-        for (int i = 0; i < E; i++) {
-            int v = in.readInt();
-            int w = in.readInt();
-            addEdge(v, w);
-        }
-    }*/
-
-   /**
-     * Copy constructor.
+    public Iterable<Integer> adj(final int v1) {
+        return adj[v1];
+    }
+    /**
+     * Determines if it has edge.
+     * complexity of this method is O(N)
+     *
+     * @param      v1     integer
+     * @param      w1     integer
+     *
+     * @return     True if has edge, False otherwise.
      */
-/*    public Graph(Graph G) {
-        this(G.V());
-        this.E = G.E();
-        for (int v = 0; v < G.V(); v++) {
-            // reverse so that adjacency list is in same order as original
-            Stack<Integer> reverse = new Stack<Integer>();
-            for (int w : G.adj[v]) {
-                reverse.push(w);
+    public boolean hasEdge(final int v1, final int w1) {
+        for (int k : adj[v1]) {
+            if (k == w1) {
+                return true;
             }
-            for (int w : reverse) {
-                adj[v].add(w);
-            }
         }
-    }*/
-
-   /**
-     * Return the number of vertices in the graph.
-     */
-    public int V() { return V; }
-
-   /**
-     * Return the number of edges in the graph.
-     */
-    public int E() { return E; }
-
-
-   /**
-     * Add the edge v-w to graph.
-     */
-    public void addEdge(int v, int w) {
-        E++;
-        adj[v].add(w);
-        adj[w].add(v);
+        return false;
     }
-
-
-   /**
-     * Return the list of neighbors of vertex v as in Iterable.
-     */
-    public Iterable<Integer> adj(int v) {
-        return adj[v];
-    }
-
-
-   /**
-     * Return a string representation of the graph.
-     */
-    public String toString() {
-        StringBuilder s = new StringBuilder();
-        String NEWLINE = System.getProperty("line.separator");
-        s.append(V + " vertices, " + E + " edges " + NEWLINE);
-        for (int v = 0; v < V; v++) {
-            s.append(v + ": ");
-            for (int w : adj[v]) {
-                s.append(w + " ");
-            }
-            s.append(NEWLINE);
-        }
-        return s.toString();
-    }
-
 }
