e7c5b3de39c67bb185c9c0c88fb295cfe9163755
prathyushab14 <prathyushabathula14@msitprogram.net>
2018-10-31 16:17:58 +0530

    "ads2_m3_a1_1: 3/8 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 0/1 "

--- ADS-2-assignments/m3/3.1 Directed Cycle/Solution.java
+++ ADS-2-assignments/m3/3.1 Directed Cycle/Solution.java
@@ -12,7 +12,7 @@ class Solution {
     	String[] tok = tokens.split(" ");
     	gph.addEdge(Integer.parseInt(tok[0]), Integer.parseInt(tok[1]));
     	// System.out.println(c.hasCycle());
-    	if (c.hasCycle() || c.hasParallelEdges(gph) || c.hasSelfLoop(gph)) {
+    	if (c.hasCycle()) {
             System.out.println("Cycle exists.");
     	} else {
     		System.out.println("Cycle doesn't exists.");
