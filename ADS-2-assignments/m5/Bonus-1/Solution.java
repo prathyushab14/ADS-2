ae3ca4482ebf4f2cdcf3c0cc34bd44a76cc283b6
prathyushab14 <prathyushabathula14@msitprogram.net>
2018-11-02 14:16:31 +0530

    "ads2_m5_ba1_1: 0/3 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 0/1 "

--- ADS-2-assignments/m5/Bonus-1/Solution.java
+++ ADS-2-assignments/m5/Bonus-1/Solution.java
@@ -7,7 +7,7 @@ class Solution {
 		String inp = sc.nextLine();
 		String[] inputs = inp.split(" ");
 		int v = Integer.parseInt(inputs[0]);
-		Graph gph = new Graph(v);
+		Graph gph = new Graph(v + 1);
 		CC connected = new CC(gph);
 		int e = Integer.parseInt(inputs[1]);
 		while (sc.hasNext()) {
@@ -15,6 +15,8 @@ class Solution {
 			String[] connects = connect.split(" ");
 			gph.addEdge(Integer.parseInt(connects[0]), Integer.parseInt(connects[1]));
 			System.out.println(connected.id(Integer.parseInt(connects[0])));
+			//int count = connected.id(Integer.parseInt(connects[0]));
+
 			//connected.id(connects[0])
         }
 
