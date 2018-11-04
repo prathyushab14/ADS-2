e1445ac9da4c953189ab671edb7f6b563597f889
prathyushab14 <prathyushabathula14@msitprogram.net>
2018-11-04 18:08:12 +0530

    "ads2_m5_ba2_1: 4/4 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 0/1 "

--- ADS-2-assignments/m5/Bonus-2/Solution.java
+++ ADS-2-assignments/m5/Bonus-2/Solution.java
@@ -3,22 +3,22 @@ import java.util.Scanner;
  * Class for edge connectivity.
  */
 class EdgeConnectivity {
-	/**
-	 * bridges variable.
-	 */
-	int   bridges; // number of bridges
-	/**
-	 * count variable.
-	 */
-    int   count;    // counter
+    /**
+     * bridges variable.
+     */
+    private int   bridges; // number of bridges
+    /**
+     * count variable.
+     */
+    private int   count;    // counter
     /**
      * preord array of integer type.
      */
-    int[] preord;    // pre[v] = order in which dfs examines v
+    private int[] preord;    // pre[v] = order in which dfs examines v
     /**
      * lowpre array of integer type.
      */
-    int[] lowpre;    // low[v] = lowest preorder of any vertex connected to v
+    private int[] lowpre;    // low[v] = lowest preorder of any vertex connected to v
     /**
      * Constructs the object.
      *
@@ -44,8 +44,7 @@ class EdgeConnectivity {
      *
      * @return     number of bridges.
      */
-    public int bridges()
-    {
+    public int bridges() {
         return bridges;
     }
     /**
@@ -55,18 +54,14 @@ class EdgeConnectivity {
      * @param      u     integer
      * @param      v     integer
      */
-    private void dfs(Graph gph, int u, int v)
-    {
+    private void dfs(final Graph gph, final int u, final int v) {
         preord[v] = count++;
         lowpre[v] = preord[v];
-        for (int w : gph.adj(v))
-        {
-            if (preord[w] == -1)
-            {
+        for (int w : gph.adj(v)) {
+            if (preord[w] == -1) {
                 dfs(gph, v, w);
                 lowpre[v] = Math.min(lowpre[v], lowpre[w]);
-                if (lowpre[w] == preord[w])
-                {
+                if (lowpre[w] == preord[w]) {
                     bridges++;
                 }
             }
@@ -75,35 +70,39 @@ class EdgeConnectivity {
             }
         }
     }
+    public int getBridges() {
+        return bridges();
+    }
 }
 /**
  * Class for solution.
  */
-class Solution {
-	/**
-	 * Constructs the object.
-	 */
-	Solution() {
+final class Solution {
+    /**
+     * Constructs the object.
+     */
+    private Solution() {
     }
     /**
      * main function.
      *
      * @param      args  The arguments
      */
-	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
-		int v = Integer.parseInt(sc.nextLine());
-		Graph gph = new Graph(v);
-		int e = Integer.parseInt(sc.nextLine());
-		while (sc.hasNext()) {
-			String[] tokens = sc.nextLine().split(" ");
-			gph.addEdge(Integer.parseInt(tokens[0]), Integer.parseInt(tokens[1]));
+    public static void main(final String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int v = Integer.parseInt(sc.nextLine());
+        Graph gph = new Graph(v);
+        int e = Integer.parseInt(sc.nextLine());
+        while (sc.hasNext()) {
+            String[] tokens = sc.nextLine().split(" ");
+            gph.addEdge(Integer.parseInt(tokens[0]),
+                Integer.parseInt(tokens[1]));
         }
         EdgeConnectivity ec = new EdgeConnectivity(gph);
         if (ec.bridges() != 0) {
-        	System.out.println(ec.bridges + " Bridges");
+            System.out.println(ec.getBridges() + " Bridges");
         } else {
-        	System.out.println("No Bridge");
+            System.out.println("No Bridge");
         }
-	}
+    }
 }
