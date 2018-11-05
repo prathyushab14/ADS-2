4063b8b0f63cc4ac2b58ae1ecc9991a07b63fe9b
prathyushab14 <prathyushabathula14@msitprogram.net>
2018-11-05 17:18:19 +0530

    "ads2_m7_a1_1: 6/6 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 0/1 "

--- ADS-2-assignments/m7/1. MST/Edge.java
+++ ADS-2-assignments/m7/1. MST/Edge.java
@@ -21,30 +21,47 @@
  *  @author Robert Sedgewick
  *  @author Kevin Wayne
  */
-public class Edge implements Comparable<Edge> { 
-
+/**
+ * Class for edge.
+ */
+public class Edge implements Comparable<Edge> {
+   /**
+    * vertex v.
+    */
     private final int v;
+    /**
+     * vertex w.
+     */
     private final int w;
+    /**
+     * weight.
+     */
     private final double weight;
 
     /**
      * Initializes an edge between vertices {@code v} and {@code w} of
      * the given {@code weight}.
      *
-     * @param  v one vertex
-     * @param  w the other vertex
-     * @param  weight the weight of this edge
-     * @throws IllegalArgumentException if either {@code v} or {@code w} 
+     * @param  v1 one vertex
+     * @param  w1 the other vertex
+     * @param  weight1 the weight of this edge
+     * @throws IllegalArgumentException if either {@code v} or {@code w}
      *         is a negative integer
      * @throws IllegalArgumentException if {@code weight} is {@code NaN}
      */
-    public Edge(int v, int w, double weight) {
-        if (v < 0) throw new IllegalArgumentException("vertex index must be a nonnegative integer");
-        if (w < 0) throw new IllegalArgumentException("vertex index must be a nonnegative integer");
-        if (Double.isNaN(weight)) throw new IllegalArgumentException("Weight is NaN");
-        this.v = v;
-        this.w = w;
-        this.weight = weight;
+    public Edge(final int v1, final int w1, final double weight1) {
+        if (v1 < 0) {
+            throw new IllegalArgumentException("vertex index must be a nonnegative integer");
+        }
+        if (w1 < 0) {
+            throw new IllegalArgumentException("vertex index must be a nonnegative integer");
+        }
+        if (Double.isNaN(weight1)) {
+            throw new IllegalArgumentException("Weight is NaN");
+        }
+        this.v = v1;
+        this.w = w1;
+        this.weight = weight1;
     }
 
     /**
@@ -66,31 +83,39 @@ public class Edge implements Comparable<Edge> {
     }
 
     /**
-     * Returns the endpoint of this edge that is different from the given vertex.
+     * Returns the endpoint of this edge that is different
+     * from the given vertex.
      *
      * @param  vertex one endpoint of this edge
      * @return the other endpoint of this edge
      * @throws IllegalArgumentException if the vertex is not one of the
      *         endpoints of this edge
      */
-    public int other(int vertex) {
-        if      (vertex == v) return w;
-        else if (vertex == w) return v;
-        else throw new IllegalArgumentException("Illegal endpoint");
+    public int other(final int vertex) {
+        if (vertex == v) {
+            return w;
+        } else if (vertex == w) {
+            return v;
+        }
+        else {
+            throw new IllegalArgumentException("Illegal endpoint");
+        }
     }
 
     /**
      * Compares two edges by weight.
      * Note that {@code compareTo()} is not consistent with {@code equals()},
-     * which uses the reference equality implementation inherited from {@code Object}.
+     * which uses the reference equality implementation 
+     * inherited from {@code Object}.
      *
      * @param  that the other edge
-     * @return a negative integer, zero, or positive integer depending on whether
+     * @return a negative integer, zero, or positive 
+     * integer depending on whether
      *         the weight of this is less than, equal to, or greater than the
      *         argument edge
      */
     @Override
-    public int compareTo(Edge that) {
+    public int compareTo(final Edge that) {
         return Double.compare(this.weight, that.weight);
     }
 }
