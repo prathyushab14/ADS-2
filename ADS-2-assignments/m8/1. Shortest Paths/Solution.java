016955b26cde2a9674b7d30b7422aa042f899896
prathyushab14 <prathyushabathula14@msitprogram.net>
2018-11-06 19:02:40 +0530

    "ads2_m8_a1_1: 0/6 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 1/1 "

---
+++ ADS-2-assignments/m8/1. Shortest Paths/Solution.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+class Solution {
+	Solution() {
+
+	}
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		String[] input = sc.nextLine().split(" ");
+		int vertices = Integer.parseInt(input[0]);
+		int edges = Integer.parseInt(input[1]);
+		String[] stations = sc.nextLine().split(" ");
+	}
+}
