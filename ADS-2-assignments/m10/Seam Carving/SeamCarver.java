cc9b6e0fc4c1c3a2246fb0275330de3d2fe3ad0f
prathyushab14 <prathyushabathula14@msitprogram.net>
2018-11-08 12:34:16 +0530

    "ads2_m10_a1_1: 2/9 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 0/1 "

---
+++ ADS-2-assignments/m10/Seam Carving/SeamCarver.java
@@ -0,0 +1,63 @@
+
+public class SeamCarver {
+	// create a seam carver object based on the given picture
+	Picture pic;
+	int w;
+	int h;
+	public SeamCarver(Picture picture) {
+		pic = picture;
+		this.w = picture.width();
+		this.h = picture.height();
+    }
+	// current picture
+	public Picture picture() {
+		return pic;
+	}
+	// width of current picture
+	public int width() {
+        return w;
+	}
+    // height of current picture
+	public int height() {
+		return h;
+	}
+    // energy of pixel at column x and row y
+	public double energy(int x, int y) {
+	    if (x == 0 || y == 0 || x == pic.width() - 1 || y == pic.height() - 1) {
+	    	return 1000;
+	    }
+	    double dx, dy = 0;
+	    double xtred = pic.get(x - 1, y).getRed();
+	    double xtblue = pic.get(x - 1, y).getRed();
+	    double xtgreen = pic.get(x - 1, y).getRed();
+	    double xbred = pic.get(x + 1, y).getRed();
+	    double xbblue = pic.get(x + 1, y).getRed();
+	    double xbgreen = pic.get(x + 1, y).getGreen();
+	    double xlred = pic.get(x, y - 1).getRed();
+	    double xlblue = pic.get(x, y - 1).getRed();
+	    double xlgreen = pic.get(x, y - 1).getRed();
+	    double xrred = pic.get(x, y + 1).getRed();
+	    double xrblue = pic.get(x, y + 1).getRed();
+	    double xrgreen = pic.get(x, y + 1).getGreen();
+	    dx = Math.pow(xtred - xbred, 2) + Math.pow(xtblue - xtblue, 2) + Math.pow(xtgreen - xbgreen, 2);
+	    dy = Math.pow(xlred - xrred, 2) + Math.pow(xlblue - xrblue, 2) + Math.pow(xlgreen - xrgreen, 2);
+	    double esum = dx + dy;
+	    return Math.sqrt(esum);
+	}
+    // sequence of indices for horizontal seam
+	public int[] findHorizontalSeam() {
+		return new int[0];
+	}
+    // sequence of indices for vertical seam
+	public int[] findVerticalSeam() {
+		return new int[0];
+	}
+    // remove horizontal seam from current picture
+	public void removeHorizontalSeam(int[] seam) {
+
+	}
+    // remove vertical seam from current picture
+	public void removeVerticalSeam(int[] seam) {
+
+	}
+}
