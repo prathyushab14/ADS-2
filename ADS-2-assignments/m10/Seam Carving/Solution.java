1627abbc59b3bd302fa30d76738d4657fd19fa2c
prathyushab14 <prathyushabathula14@msitprogram.net>
2018-11-08 13:50:22 +0530

    "ads2_m10_a1_1: 5/9 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 0/1 "

--- ADS-2-assignments/m10/Seam Carving/Solution.java
+++ ADS-2-assignments/m10/Seam Carving/Solution.java
@@ -46,6 +46,7 @@ public class Solution {
         SeamCarver seamCarver = null;
         if (!scan.hasNext()) {
             System.out.println("picture is null");
+            return;
         }
         try {
             switch (cases) {
