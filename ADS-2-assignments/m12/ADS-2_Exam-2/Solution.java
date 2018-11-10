5a9a949f70aaeebc446aadd21cd78d7a014e2696
prathyushab14 <prathyushabathula14@msitprogram.net>
2018-11-10 12:55:21 +0530

    "ads2_m12_a1_1: 4/6 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 1/1 "

--- ADS-2-assignments/m12/ADS-2_Exam-2/Solution.java
+++ ADS-2-assignments/m12/ADS-2_Exam-2/Solution.java
@@ -1,10 +1,6 @@
 import java.util.Scanner;
 public class Solution {
-
-	public static void main(String[] args) {
-		// Self loops are not allowed...
-		// Parallel Edges are allowed...
-		// Take the Graph input here...
+    public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		int no_v = Integer.parseInt(sc.nextLine());
 		EdgeWeightedGraph ewg = new EdgeWeightedGraph(no_v);
@@ -14,18 +10,14 @@ public class Solution {
 		    Edge e = new Edge(Integer.parseInt(str[0]), Integer.parseInt(str[1]), Double.parseDouble(str[2]));
 		    ewg.addEdge(e);
 		}
+		while (sc.hasNext()) {
         String caseToGo = sc.nextLine();
 		switch (caseToGo) {
 		case "Graph":
-			//Print the Graph Object.
 		    System.out.println(no_v + " vertices " + no_e + " edges");
             System.out.println(ewg);
             break;
         case "DirectedPaths":
-			// Handle the case of DirectedPaths, where two integers are given.
-			// First is the source and second is the destination.
-			// If the path exists print the distance between them.
-			// Other wise print "No Path Found."
 			String[] tok = sc.nextLine().split(" ");
 			int so = Integer.parseInt(tok[0]);
 			int de = Integer.parseInt(tok[1]);
@@ -36,15 +28,7 @@ public class Solution {
 				System.out.println("No Path Found.");
 			}
 			break;
-
-	
-
-		case "ViaPaths":
-			// Handle the case of ViaPaths, where three integers are given.
-			// First is the source and second is the via is the one where path should pass throuh.
-			// third is the destination.
-			// If the path exists print the distance between them.
-			// Other wise print "No Path Found."
+        case "ViaPaths":
 			String[] inp = sc.nextLine().split(" ");
 			int sou = Integer.parseInt(inp[0]);
 			int via = Integer.parseInt(inp[1]);
@@ -59,6 +43,7 @@ public class Solution {
 		default:
 			break;
 		}
+	}
 
 	}
 }
