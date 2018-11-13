6c92aecb377ec29ace0200cdb1b4636814bbd3ac
prathyushab14 <prathyushabathula14@msitprogram.net>
2018-11-13 22:46:56 +0530

    "ads2_m14_a1_1: 6/6 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 1/1 "

--- ADS-2-assignments/m14/Assignment-1/Solution.java
+++ ADS-2-assignments/m14/Assignment-1/Solution.java
@@ -1,28 +1,60 @@
+/**
+ * Scanner import.
+ */
 import java.util.Scanner;
-
-public class Solution {
-	public static void main(String[] args) {
-		String[] words = loadWords();
-		//Your code goes here...
-		Scanner sc = new Scanner(System.in);
-		String s = sc.nextLine();
-		TST<Integer> tst = new TST<Integer>();
-		for (int i = 0; i < words.length; i++) {
-			for (int j = 0; j < words[i].length(); j++) {
-				String c = words[i].substring(j);
-				if (!tst.contains(c)) {
-					tst.put(c, i);
-				}
-			}
-			// tst.put(words[i], i);
-		}
-		for (String str : tst.keysWithPrefix(s)) {
-			System.out.println(str);
-		}
-	}
+/**
+ * Class for solution.
+ */
+public final class Solution {
+    /**
+     * Constructs the object.
+     */
+    private Solution() {
+        //Empty constructor.
+    }
+    /**
+     * Main function.
+     *
+     * @param      args  The arguments
+     */
+    public static void main(final String[] args) {
+        String[] words = loadWords();
+        Scanner sc = new Scanner(System.in);
+        //Your code goes here...
+        TST<Integer> tst = new TST<Integer>();
+        /**
+         * Putting the words into tst.
+         *
+         * Complexity is N^2.
+         */
+        for (int i = 0; i < words.length; i++) {
+            for (int j = 0; j < words[i].length(); j++) {
+                String s = words[i].substring(j);
+                if (!tst.contains(s)) {
+                    tst.put(s, i);
+                }
+            }
+        }
+        String prefix = sc.nextLine();
+        /**
+         * Printing all the 9
+         */
+        for (String str : tst.keysWithPrefix(prefix)) {
+            System.out.println(str);
+        }
+    }
+    /**
+     * Loads words.
+     *
+     * @return     { description_of_the_return_value }
+     */
     public static String[] loadWords() {
-		In in = new In("/Files/dictionary-algs4.txt");
-		String[] words = in.readAllStrings();
-		return words;
-	}
+        In in = new In("/Files/dictionary-algs4.txt");
+        String[] words = in.readAllStrings();
+        return words;
+    }
 }
+
+
+
+
